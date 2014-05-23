dab67e413dbb14b83318ae33ab5fae78a8a473e7
Max Gu <guxinchi2000@gmail.com>
2014-05-23 13:01:46 -0700

    add worker client

--- map_fold/src/edu/ucsd/map_fold/common/ControllerClient.java
+++ map_fold/src/edu/ucsd/map_fold/common/ControllerClient.java
@@ -5,11 +5,22 @@ import java.rmi.server.*;
  * Created by max on 5/20/14.
  */
 public class ControllerClient {
-    public static void main (String[] argv){
-        try{
+//    public static void main (String[] argv){
+//        try{
+//
+//        }catch(Exception e){
+//
+//        }
+//    }
 
-        }catch(Exception e){
-
+    public static WorkerInterface connectToController(String IP) {
+        try {
+            ControllerInterface controller =
+                    (WorkerInterface) Naming.lookup (IP+"/Controller");
+            // (HelloInterface) Naming.lookup ("/Hello2");
+            return controller;
+        } catch (Exception e) {
+            System.out.println ("ControllerClient exception: " + e);
         }
     }
 }
--- map_fold/src/edu/ucsd/map_fold/common/WorkerClient.java
+++ map_fold/src/edu/ucsd/map_fold/common/WorkerClient.java
@@ -5,11 +5,21 @@ import java.rmi.server.*;
  * Created by max on 5/20/14.
  */
 public class WorkerClient {
-    public static void main (String[] argv){
-        try{
-
-        }catch(Exception e){
-
+//    public static void main (String[] argv){
+//        try{
+//
+//        }catch(Exception e){
+//
+//        }
+//    }
+    public static WorkerInterface connectToWorker(String IP) {
+        try {
+            WorkerInterface worker =
+                    (WorkerInterface) Naming.lookup (IP+"/Worker");
+            // (HelloInterface) Naming.lookup ("/Hello2");
+            return worker;
+        } catch (Exception e) {
+            System.out.println ("WorkerClient exception: " + e);
         }
     }
 }
