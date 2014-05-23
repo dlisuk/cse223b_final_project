2c2b12a8c995b1bb718ce3fec8d9431235907c02
Max Gu <guxinchi2000@gmail.com>
2014-05-22 17:55:17 -0700

    Created worker server

--- map_fold/src/edu/ucsd/map_fold/common/WorkerServer.java
+++ map_fold/src/edu/ucsd/map_fold/common/WorkerServer.java
@@ -1,15 +1,21 @@
 package edu.ucsd.map_fold.common;
 import java.rmi.*;
 import java.rmi.server.*;
+import edu.ucsd.map_fold.worker;
+import edu.ucsd.map_fold.worker.WorkerNode;
+
 /**
  * Created by max on 5/20/14.
  */
 public class WorkerServer {
     public static void main (String[] argv){
         try{
+            WorkerNode worker = new WorkerNode();
+            worker.start();
+            Naming.rebind ("worker", worker);
 
         }catch(Exception e){
-
+            System.out.println ("Hello Server failed: " + e);
         }
     }
 }
