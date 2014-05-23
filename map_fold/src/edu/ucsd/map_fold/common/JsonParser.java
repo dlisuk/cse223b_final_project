af44de597d4d6ebb6b97dee4d97907299ff23238
dotcom900825 <dotcom900825@gmail.com>
2014-05-23 12:51:22 -0700

    May 23th version controller server and controller node

---
+++ map_fold/src/edu/ucsd/map_fold/common/JsonParser.java
@@ -0,0 +1,76 @@
+package edu.ucsd.map_fold.common;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import edu.ucsd.map_fold.common.logistic_regression.Token;
+
+public class JsonParser {
+
+    public JsonParser(String conf_path) throws IOException, ParseException{
+       this.parser = new JSONParser();
+       this.nodeData = (JSONObject) parser.parse(new FileReader(conf_path));
+    }
+
+	public int parseControllerPort(){
+
+        JSONArray controllerArray = (JSONArray)nodeData.get("controller");
+    	
+        JSONObject node = (JSONObject)controllerArray.get(0);
+    	int port = (Integer)node.get("port");
+    	
+		return port;
+	}
+
+    public int parseWorkerNum(){
+        int workerNum = (Integer)nodeData.get("workers");
+        return workerNum;
+    }
+
+    public String parseDataPath(){
+        String dataPath = (String)nodeData.get("data");
+        return dataPath;
+    }
+
+    public List parseWorkerAddr(){
+        JSONArray nodeArray = (JSONArray)nodeData.get("worker");
+        List workerAddrList = new ArrayList<>();
+        for(Object o : nodeArray){
+            JSONObject node = (JSONObject) o;
+            String addr = (String)node.get("addr");
+            int threads = (Integer)node.get("threads");
+            workerAddrList.add(addr);
+        }
+        return workerAddrList;
+
+    }
+
+    public List parseTokens(){
+        JSONArray tokenArray = (JSONArray)nodeData.get("tokens");
+        List tokenList = new ArrayList<>();
+        for(Object o : tokenArray){
+            JSONObject token = (JSONObject) o;
+            double mu = (Double)token.get("mu");
+            double lambda = (Double)token.get("lambda");
+            Integer[] fields = (Integer[])token.get("fields");
+            List<Integer> fieldList = Arrays.asList(fields);
+            Random tokenIdGen = new Random();
+            Token tokenObject = new Token(tokenIdGen.nextInt(), fieldList, mu, lambda);
+            tokenList.add(tokenObject);
+        }
+        return tokenList;
+    }
+
+    private JSONParser parser;
+    private JSONObject nodeData;
+}
--- map_fold/src/edu/ucsd/map_fold/controller/ControllerNode.java
+++ map_fold/src/edu/ucsd/map_fold/controller/ControllerNode.java
@@ -2,27 +2,57 @@ package edu.ucsd.map_fold.controller;
 
 import edu.ucsd.map_fold.common.ControllerInterface;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import edu.ucsd.map_fold.common.JsonParser;
 
 /* ControllerNode -- Main class to launch a controller node */
 public class ControllerNode extends UnicastRemoteObject implements ControllerInterface {
-//    public static void main(String[] args) {
-//        System.out.println("Hello Max");
-//
-//    }
-//    TODO:Controller
-    public ControllerNode() throws RemoteException{
+
+    //    TODO:Controller
+    public ControllerNode(String jobConf, String workerConf) throws FileNotFoundException, IOException, ParseException{
+        JsonParser parser = new JsonParser(jobConf);
+        this.workerNum = parser.parseWorkerNum();
+        this.dataPath = parser.parseDataPath();
+        this.workerList = parser.parseWorkerAddr();
 
     }
     public Boolean DoneWithWork() throws RemoteException{
         return true;
     }
+
     public Boolean tokenReceived(int tokenId, int tokenVersion){
+        // TODO load data to clients
         return true;
     }
     public Boolean dataLoaded(String filePath, int offset, int count){
+        // TODO clients start work
+        
         return true;
     }
+
+    public void run(){
+        //TODO read the data set and divide the works
+
+        //TODO upload token to all clients
+
+
+    }
+
+    public List<String> workerList;
+    public List controllerList;
+    public int workerNum;
+    public String dataPath;
+
+
 }
